f9a445a7a755f23b3ae4b5a59a90c2241245a2b3
xiaoba <liuchang0318@autohome.com.cn>
2022-07-05 09:41:11 +0800

    update

--- src/api/tenantInfoSave.js
+++ src/api/tenantInfoSave.js
@@ -1,15 +1,25 @@
 // let host = 'https://30.100api.cn/' // 开发API地址
 // let host = 'https://liveapi.cn/' // 测试API地址
-let host = 'https://api.cmalive.cn/' // 线上API地址
+// let host = 'https://api.cmalive.cn/' // 线上API地址
+// const href = window.location.href
+// if (href.indexOf('localhost') >= 0 || window.location.port === '9004') {
+//   host = '' // 本地调试走代理
+// } else if (href.indexOf('.test.') >= 0) {
+//   host = 'https://30.100api.cn/'
+// } else if (href.indexOf('.100live.') >= 0 || href.indexOf('saas-test.') >= 0) {
+//   host = 'https://liveapi.cn/'
+// } else if (href.indexOf('.100doc.') >= 0) {
+//   host = 'https://api.cmalive.cn/'
+// }
+
+const host = '';
 const href = window.location.href
-if (href.indexOf('localhost') >= 0 || window.location.port === '9004') {
-  host = '' // 本地调试走代理
-} else if (href.indexOf('.test.') >= 0) {
-  host = 'https://30.100api.cn/'
-} else if (href.indexOf('.100live.') >= 0 || href.indexOf('saas-test.') >= 0) {
-  host = 'https://liveapi.cn/'
-} else if (href.indexOf('.100doc.') >= 0) {
-  host = 'https://api.cmalive.cn/'
+  if (href.indexOf('localhost') >= 0 || (href.indexOf('.dev.')) >= 0) {
+      hostUrl = 'https://gsk.dev.100url.cn/' // 本地调试走代理
+  } else if (href.indexOf('.test.') >= 0) {
+      hostUrl = 'https://gsk.test.100url.cn/'
+  } else {
+      //hostUrl = 'https://api.cmalive.cn/'
 }
 
 const getSig = `${host}api/yb-resource-api-ntk/`
