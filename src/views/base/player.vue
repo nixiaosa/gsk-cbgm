3f89961973b9994b59e5b28dc41266192bf21845
xiaoba <liuchang0318@autohome.com.cn>
2022-08-18 12:31:03 +0800

    u

--- src/views/base/player.vue
+++ src/views/base/player.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="base-set">
     <audio v-if="videoInfo.programType" :src="videoInfo.audioUrl" controls/>
-    <video v-if="videoInfo.programType" :src="videoInfo.playbackUrl" controls/>    
+    <video v-show="show" :src="videoInfo.playbackUrl" controls/>
     <!-- <div class="videoBox" v-show="show">
       <div id="id_test_video" style="width:100%; height:auto;"></div>
     </div>
